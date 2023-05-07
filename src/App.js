6c0acadcb3ca48dc9e6935b2f6dae25fcd2c9dfb
abich1 <chinabi@uw.edu>
2023-05-07 14:30:17 -0700

    add intro react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Intro from "./components/intro";
+import './App.css'
+
+
+export default function App() {
+    return (
+    <div>
+        <Intro />
+    </div>
+    )
+}
