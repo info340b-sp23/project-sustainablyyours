5651de8c1f6698c5c5d9acc62b9a49f3aa6718ba
abich1 <chinabi@uw.edu>
2023-05-07 14:49:37 -0700

    add ip header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Intro from "./components/intro";
+import Header from "./components/header";
 import './App.css'
 
 
 export default function App() {
     return (
     <div>
+        <Header />
         <Intro />
     </div>
     )
