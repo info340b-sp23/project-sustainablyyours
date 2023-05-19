b8241fd5e977883a1f9fa8d9ec12677097ac49da
abich1 <chinabi@uw.edu>
2023-05-19 01:03:50 -0700

    footer social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,11 @@ export function Footer() {
         <div className="footer-basic border-box">
         <footer className="bg-light text-center">
             <div className="social text-center p-3">
-              <a href="intro.html" aria-label="Home"><i className="fa-solid fa-house"></i></a>
-              <a href="about.html" aria-label="About Us"><i className="fa-solid fa-address-card"></i></a>
-              <a href="index.html" aria-label="Shop"><i className="fa-solid fa-store"></i> </a>
-              <a href="contact.html" aria-label="Contact"><i className="fa-solid fa-address-book"></i></a>
-              <a href="account.html" aria-label="Account Login"><i className="fa-solid fa-right-to-bracket"></i></a>
+              <a href="intro" aria-label="Home"><i className="fa-solid fa-house"></i></a>
+              <a href="about" aria-label="About Us"><i className="fa-solid fa-address-card"></i></a>
+              <a href="shop" aria-label="Shop"><i className="fa-solid fa-store"></i> </a>
+              <a href="contact" aria-label="Contact"><i className="fa-solid fa-address-book"></i></a>
+              <a href="account" aria-label="Account Login"><i className="fa-solid fa-right-to-bracket"></i></a>
             </div>
             <p className="copyright">SustainablyYours  &copy;2023</p>
         </footer>
